c3b50b255a2836cb0bd5961a33d5ad85834641e3
therealedsheenan <private.sheenantenepre@gmail.com>
2017-05-23 13:52:50 +0900

    add dependencies

--- src/client/containers/Styleguide/StyleguideContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const StyleguideContainer = () => (
-  <div className="Styleguide">
-    <p>this is the styleguide</p>
-  </div>
-  );
-
-export default StyleguideContainer;
