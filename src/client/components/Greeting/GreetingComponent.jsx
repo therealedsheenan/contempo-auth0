c77bb085f63cecf2dd96b3f4a903a9539b901969
therealedsheenan <private.sheenantenepre@gmail.com>
2017-05-23 12:05:51 +0900

    add contempo main files

---
+++ src/client/components/Greeting/GreetingComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Title,
+  SubTitle,
+  Wrapper
+} from './styles';
+
+
+const GreetingComponent = props => (
+  <Wrapper>
+    <Title>Good {props.message}!</Title>
+    <SubTitle>Welcome to</SubTitle>
+    <div>
+      <img style={{ width: '100%' }} src="./assets/images/contempo.png" alt="" />
+    </div>
+  </Wrapper>
+  );
+
+GreetingComponent.defaultProps = {
+  message: 'No message passed.'
+};
+
+GreetingComponent.propTypes = {
+  message: PropTypes.string
+};
+
+export default GreetingComponent;
